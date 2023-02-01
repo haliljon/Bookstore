12e0be9b1f10a290723ac0674a7f9aa560e90368
haliljon <110017001+haliljon@users.noreply.github.com>
2023-02-01 12:04:12 +0500

    made minor changes

--- src/redux/book/book.js
+++ src/redux/book/book.js
@@ -42,7 +42,7 @@ const bookReducer = (state = initialBookState, action) => {
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return [...state.filter((book) => book.id !== action.id)];
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }

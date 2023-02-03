ee297c6580284aca20e743505469697ce57c928f
haliljon <110017001+haliljon@users.noreply.github.com>
2023-02-04 03:56:09 +0500

    Added design

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
+import underConstruction from '../assets/Page_Under_Construction.jpg';
 
 const Categories = () => {
   const dispatch = useDispatch();
@@ -8,8 +9,9 @@ const Categories = () => {
     dispatch(checkStatus());
   };
   return (
-    <div>
+    <div className="categories">
       <h1>{status.message}</h1>
+      <img src={underConstruction} alt="Under construction" style={status.img} />
       <button
         type="button"
         className="primary-button-big"
